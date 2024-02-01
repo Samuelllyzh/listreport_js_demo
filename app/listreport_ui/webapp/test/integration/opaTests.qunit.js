bd5590d880f18cde9c81cba451dfa4ad1b6d4f4d
Yo Ri <yo.ri@toppan.co.jp>
2024-02-01 06:27:33 +0000

    Fiori Elements ListReportプロジェクト作成

---
+++ app/listreport_ui/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,26 @@
+sap.ui.require(
+    [
+        'sap/fe/test/JourneyRunner',
+        'listreportui/test/integration/FirstJourney',
+		'listreportui/test/integration/pages/BooksList',
+		'listreportui/test/integration/pages/BooksObjectPage'
+    ],
+    function(JourneyRunner, opaJourney, BooksList, BooksObjectPage) {
+        'use strict';
+        var JourneyRunner = new JourneyRunner({
+            // start index.html in web folder
+            launchUrl: sap.ui.require.toUrl('listreportui') + '/index.html'
+        });
+
+       
+        JourneyRunner.run(
+            {
+                pages: { 
+					onTheBooksList: BooksList,
+					onTheBooksObjectPage: BooksObjectPage
+                }
+            },
+            opaJourney.run
+        );
+    }
+);
