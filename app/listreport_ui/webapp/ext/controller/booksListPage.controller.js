6a1dfe931544788350bb201414ee99465b9d9618
Samuelllyzh <yzh.samuel97@gmail.com>
2024-04-15 07:56:01 +0000

    Rename controller.js

--- app/listreport_ui/webapp/ext/controller/booksListPage.controller.js
+++ app/listreport_ui/webapp/ext/controller/booksListPage.controller.js
@@ -1,13 +1,13 @@
 sap.ui.define(['sap/ui/core/mvc/ControllerExtension'], function (ControllerExtension) {
 	'use strict';
 
-	return ControllerExtension.extend('listreportui.ext.controller.BooksListContro', {
+	return ControllerExtension.extend('listreportui.ext.controller.booksListPage', {
 		// this section allows to extend lifecycle hooks or hooks provided by Fiori elements
 		override: {
 			/**
 			 * Called when a controller is instantiated and its View controls (if available) are already created.
 			 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-			 * @memberOf listreportui.ext.controller.BooksListContro
+			 * @memberOf listreportui.ext.controller.booksListPage
 			 */
 			onInit: function () {
 				// you can access the Fiori elements extensionAPI via this.base.getExtensionAPI
